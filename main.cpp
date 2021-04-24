7b65d7b1c79a3bec09a4230161efff6728000c8a
Paweł <heropn754@gmail.com>
2021-04-24 12:59:27 +0200

    Added sfml to vs project and created sample window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	std::cin.get();
+	sf::ContextSettings settings;
+	settings.antialiasingLevel = 8;
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Snake Game", sf::Style::Close, settings);
+	window.setVerticalSyncEnabled(true);
+	window.setKeyRepeatEnabled(false);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		window.clear();
+		//Draw here
+		window.display();
+	}
 }
